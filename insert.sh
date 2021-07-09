fc2e1e423e1e66a70e1421bc0225827046b3be0d
Naresh Maharaj <naresh.maharaj@icloud.com>
2021-07-09 14:26:36 +0100

    Refactor of jar file name and run insert script

--- insert.sh
+++ insert.sh
@@ -3,4 +3,4 @@ INSERTS=$2
 HOSTS_CSV=$3
 NAMESPACE=$4
 SET=$5
-java -jar out/artifacts/async_jar/async.jar $THREADS $INSERTS $HOSTS_CSV $NAMESPACE $SET
+java -jar out/artifacts/aerospike_threads_async_jar/aerospike_threads_async.jar $THREADS $INSERTS $HOSTS_CSV $NAMESPACE $SET
