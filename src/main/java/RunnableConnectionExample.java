a56396f0a4e1c30001b4010b52c195b9f54f848a
Naresh Maharaj <nareshmaharaj@Nareshs-MacBook-Pro.local>
2021-07-09 13:03:16 +0100

    Added executable Jar file

--- src/main/java/RunnableConnectionExample.java
+++ src/main/java/RunnableConnectionExample.java
@@ -1,5 +1,6 @@
 import com.aerospike.client.AerospikeClient;
 import com.aerospike.client.Bin;
+import com.aerospike.client.Host;
 import com.aerospike.client.Key;
 import com.aerospike.client.async.Monitor;
 
@@ -7,15 +8,63 @@ import java.util.concurrent.atomic.AtomicLong;
 
 public class RunnableConnectionExample
 {
+
     static AtomicLong start = new AtomicLong(0);
-    static int numberOfClients = 100;
-    static int numberOfRecords = 100000;
+    static int numberOfClients = 10;
+    static int numberOfRecords = 1000;
+    static int port = 3000;
+
+    static Host[] hosts = new Host[] {
+            new Host("127.0.0.1", port)
+    };
+
+    static String namespace = "insurance";
+    static String set = "exp1";
+
     static Monitor monitor = new Monitor();
 
+    /*
+        java -jar async.jar threads records "host1,host2,host3" ns set"
+        java -jar async.jar 100 1000 "10.0.0.61,10.0.1.33,10.0.2.147" test set1"
+     */
     public static void main(String [] args)
     {
+        if ( args.length == 1 ) {
+            System.out.println(
+                    "e.g. java -jar async.jar threads records \"host1,host2,host3\" ns set\n" +
+                    "e.g. java -jar async.jar 100 1000 \"10.0.0.61,10.0.1.33,10.0.2.147\" test set1");
+            System.exit(0);
+        }
+        if ( args.length >= 2 ) {
+            numberOfClients = Integer.parseInt(args[0]);
+            numberOfRecords = Integer.parseInt(args[1]);
+        }
+        if ( args.length >= 3 ) {
+
+            String [] listOfIps = args[2].split(",");
+            Host[] tmpHost = new Host[listOfIps.length];
+            for (int i = 0; i < listOfIps.length; i++) {
+                tmpHost[i] =  new Host(listOfIps[i], port);
+            }
+            hosts = tmpHost;
+        }
+
+        if ( args.length >= 4 ) {
+            namespace = args[3];
+        }
+
+        if ( args.length >= 5 ) {
+            set = args[4];
+        }
+
         int numberOfRecordsPerThread = numberOfRecords / numberOfClients;
         long startTime = System.currentTimeMillis();
+
+        System.out.println(
+                "NumberOfClient Threads:" + numberOfClients +
+                "\nNumberOfRecords to Insert:" + numberOfRecords
+        );
+        // System.exit(0);
 
         for (int i = 0; i < numberOfClients; i++ )
         {
@@ -24,7 +73,9 @@ public class RunnableConnectionExample
                             "Aerospike Connection: ".concat(Integer.toString(i)),
                             numberOfRecordsPerThread,
                             i * numberOfRecordsPerThread,
-                            monitor
+                            monitor,
+                            namespace,
+                            set
             );
             R1.start();
         }
@@ -37,6 +88,8 @@ public class RunnableConnectionExample
 
 class RunnableConnection implements Runnable {
 
+    private final String namespace;
+    private final String set;
     private int numberOfDocuments;
     private int startKey;
     private Thread t;
@@ -44,12 +97,16 @@ class RunnableConnection implements Runnable {
     AerospikeClient client;
     Monitor monitor;
 
-    RunnableConnection(String name, int numOfRecords, int startKey, Monitor monitor) {
+    RunnableConnection(String name, int numOfRecords, int startKey, Monitor monitor, String namespace, String set) {
         threadName = name;
-        this.client = new AerospikeClient(null, "localhost", 3000);;
+        this.client = new AerospikeClient(
+                null, RunnableConnectionExample.hosts)
+        ;
         this.numberOfDocuments = numOfRecords;
         this.startKey = startKey;
         this.monitor = monitor;
+        this.namespace = namespace;
+        this.set = set;
     }
 
     @Override
@@ -70,7 +127,7 @@ class RunnableConnection implements Runnable {
         int rand1 = (int)r1;
         int rand2 = (int)r2;
 
-        Key key = new Key("insurance", "exp2", keyIndex);
+        Key key = new Key(namespace, set, keyIndex);
         Bin bin = new Bin("bin", keyIndex);
 
         Bin binType = new Bin("val1", rand1);
